549987b6253cb43651f22c4610f3f562e594be50
rodrigoglisciak <rodrigoalejoglisciak@gmail.com>
2022-02-06 01:01:11 -0300

    numero 2

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const app = express();
-const port =  10000;
+const port =  1234;
+
+
+
+// 
+
+app.set('views', './vistas');
+app.set('view engine' , 'ejs');
 
 
 
 
+// ARCHIVOS ESTATICOS
+app.use(express.static('./vistas'));
+app.use(express.static('./src'));
+app.use(express.static('./estilos'));
+
+
+
+
+// RUTAS DE LA WEB 
+app.use('/', require('./rutas/rutasWeb'));
 
 
 app.listen(port, ()  => {
 
-    console.log('estoy escuchando en el puerto' + port);
+    console.log('estoy escuchando en el puerto ' + port);
 
 
 } );
